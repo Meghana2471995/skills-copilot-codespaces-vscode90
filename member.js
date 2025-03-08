ebb446892d6139c1ee2ab84e033f558f7810e9ad
Meghana2471995 <meghana.mallam@accenture.com>
2025-03-08 09:27:55 +0000

    copilot send commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    const member = {
+        name: 'Jeniffer',
+        age: 25,
+        skills: ['HTML', 'CSS', 'JS', 'React', 'Node']
+    };
+
+    return member.skills;
+}
